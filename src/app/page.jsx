c0472a8e70e72438dad00f8b8f7254b7bbf47175
rifki achmad fachrezy <rifkiachmadfachrezy@gmail.com>
2025-02-04 02:07:33 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default async function Home() {
+  return (
+    <>
+      <div>
+        <Button>te</Button>
+      </div>
+    </>
+  );
+}
---
+++ src/lib/db.js
@@ -0,0 +1,10 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = globalThis.prisma || new PrismaClient();
+
+// Only set the global prisma in development to avoid excessive Prisma client instances in production
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
+}
+
+export const db = prisma;
